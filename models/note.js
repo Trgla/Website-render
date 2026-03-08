ce32fda064900c2b7e6935977e14580beec09c56
Trang <trangvu197@gmail.com>
2026-03-08 17:49:17 +0200

    updates

---
+++ models/note.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI 
+//To define the value of an environment variable if there is no .env file,
+//run command: MONGODB_URI="your_connection_string_here" npm run dev
+//.env file is already made so it's not neccesary anymore.
+
+console.log('connecting to', url)
+mongoose.connect(url, { family: 4 })
+
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Note', noteSchema)
+
+//command: node --watch index.js yourusername yourpassword
